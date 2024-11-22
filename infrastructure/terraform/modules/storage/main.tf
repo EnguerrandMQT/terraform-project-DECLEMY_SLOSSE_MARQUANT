7581fc09a110c3347df932e19a3c6e7ea3227354
Enguerrand <enguerrand.marquant@student.junia.com>
2024-11-22 09:56:56 +0100

    chore(storage): add bypass rules for Azure Services + update deprecated code
    Signed-off-by: Enguerrand <enguerrand.marquant@student.junia.com>

--- infrastructure/terraform/modules/storage/main.tf
+++ infrastructure/terraform/modules/storage/main.tf
@@ -8,22 +8,18 @@ resource "azurerm_storage_account" "storage" {
 
 resource "azurerm_storage_container" "container" {
   name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.storage.name
+  storage_account_id    = azurerm_storage_account.storage.id
   container_access_type = "private"
 }
 
 resource "azurerm_role_assignment" "service_binding" {
- # count = var.service_principal_id != null ? 1 : 0
-
-  scope                = resource.azurerm_storage_container.container.resource_manager_id
+  scope                = azurerm_storage_container.container.resource_manager_id
   role_definition_name = "Storage Blob Data Reader"
   principal_id         = var.service_principal_id
 }
 
 resource "azurerm_role_assignment" "user_binding" {
-  count = var.user_principal_id != null ? 1 : 0
-
-  scope                = resource.azurerm_storage_container.container.resource_manager_id
+  scope                = azurerm_storage_container.container.resource_manager_id
   role_definition_name = "Storage Blob Data Reader"
   principal_id         = var.user_principal_id
 }
@@ -40,6 +36,7 @@ resource "azurerm_storage_account_network_rules" "network_rules" {
   storage_account_id = azurerm_storage_account.storage.id
 
   default_action = "Deny"
+  bypass = ["AzureServices", "Logging", "Metrics"]
 
   virtual_network_subnet_ids = [
     var.storage_subnet_id
